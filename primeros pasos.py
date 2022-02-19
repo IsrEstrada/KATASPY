d63b65ef324d62c79679228e278e39993aeef173
IsrEstrada <99391041+IsrEstrada@users.noreply.github.com>
2022-02-18 23:23:23 -0600

    Add files via upload

---
+++ primeros pasos.py
@@ -0,0 +1,9 @@
+#python3 primeros pasos.py
+print("Hola desde la consola")
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+#Declaramos la variable
+distancia_alfa_centauri = 4.367
+# Descubrimos su tipo de dato
+type(distancia_alfa_centauri)
